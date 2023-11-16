4568eccb1ca0b4f592145389806a0b56815a9f2f
JasonD0811 <75908196+JasonD0811@users.noreply.github.com>
2023-11-16 16:04:37 -0500

    Removed code to populate database from backend

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
+const Song = require('./models/songSchema');
+const musicianData = require('./models/musicianSchema');
+const djData = require('./models/djSchema');
+const fs = require('fs');
 
 // MongoDB Atlas connection URI
 const mongoURI =
@@ -14,44 +18,72 @@ const client = new MongoClient(mongoURI, {
     deprecationErrors: true,
   },
 });
+  // Get Mongoose connection
+  const mongooseConnection = mongoose.connection;
 
 // Connect to MongoDB using MongoClient
-async function connectMongoDB() {
-  try {
-    await client.connect();
-    await client.db('admin').command({ ping: 1 });
-    console.log('Connected to MongoDB successfully!');
-  } catch (error) {
-    console.error('Connection Error:', error);
-    process.exit(1); // Exit the process if there's an error connecting to MongoDB
-  }
+ async function connectMongoDB() {
+  
+     mongoose.connect(mongoURI, {dbName: 'Radio-Station-DB'}); // Mongoose connection to MongoDB
+    
+     // Listen for Mongoose connection events
+      
+      mongooseConnection.on('open', function () {
+      console.log('Mongoose connected to MongoDB Successfully!');
+      });
+      mongooseConnection.on("close", function(){
+      console.log("Mongoose connection closed");
+      });
+      mongooseConnection.on('error', console.error.bind(console, 'Mongoose Connection Error:'));
+  
 }
+connectMongoDB();
 
-// Mongoose connection to MongoDB
-mongoose.connect(mongoURI);
-
-// Get Mongoose connection
-const mongooseConnection = mongoose.connection;
-
-// Listen for Mongoose connection events
-mongooseConnection.on('error', console.error.bind(console, 'Mongoose Connection Error:'));
-mongooseConnection.once('open', function () {
-  console.log('Mongoose connected to MongoDB Successfully!');
-});
 
 const app = express();
 app.use(express.json());
 
-// Close connections and exit the process after 5 seconds
-function closeConnections() {
-  client.close();
-  mongooseConnection.close();
-  console.log('Connections closed. Exiting...');
-  process.exit(0);
-}
+// // Close connections and exit the process after 5 seconds
+// function closeConnections() {
+//   client.close();
+//   mongooseConnection.close();
+//   console.log('Connections closed. Exiting...');
+//   process.exit(0);
+// }
 
 // Gracefully close connections and terminate the process after 5 seconds
-setTimeout(closeConnections, 5000); // Adjust the timeout as needed
+//setTimeout(closeConnections, 5000); // Adjust the timeout as needed
+
+// // Use JSON file containing information about songs to populate an array songs, which will then be inserted into the database
+// const songs = JSON.parse(fs.readFileSync('../SWE432/data/songData.json','utf8'));
+
+// //use Mongoose's insertMany method to populate the songs array in the database
+// Song.insertMany(songs)
+// .then(function(songs){
+//   console.log('Songs saved: ',songs);
+// })
+// .catch(function(err){
+//   console.log(err);
+// });
+
+// const musicians = JSON.parse(fs.readFileSync('../SWE432/data/musicianData.json','utf8'));
+// musicianData.insertMany(musicians)
+// .then(function(musicians){
+//   console.log('Saved dat of these musicians: ', musicians);
+// })
+// .catch(function(err){
+//   console.log(err);
+// });
+
+// const djShowInfo = JSON.parse(fs.readFileSync('../SWE432/data/djData.json','utf8'));
+// djData.insertMany(djShowInfo)
+// .then(function(djShowInfo){
+//   console.log("Saved data of these DJ's: ", djShowInfo);
+// })
+// .catch(function(err){
+//   console.log(err);
+// });
+
 
 // Expose the app for potential future use
 module.exports = app;
